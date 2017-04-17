3ce8e0b9380d33795e42615f419c2c252667efcd
DevBeus <elmar118@gmail.com>
2017-04-17 21:26:07 +0200

    Another change

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 //Comment by DevBeus on master
+//Another change
 
 public class MainActivity extends AppCompatActivity {
 
